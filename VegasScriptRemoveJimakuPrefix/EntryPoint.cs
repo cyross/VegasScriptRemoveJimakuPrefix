e8bdadb029eef5b810dab17453da9202da1e1377
Cyross Makoto <takuan45189@gmail.com>
2023-01-25 02:35:14 +0900

    first commit

---
+++ VegasScriptRemoveJimakuPrefix/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using ScriptPortal.Vegas;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VegasScriptRemoveJimakuPrefix
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            VegasScriptSettings.Load();
+            VegasHelper helper = VegasHelper.Instance(vegas);
+
+            try
+            {
+                helper.DeleteJimakuPrefix();
+            }
+            catch (VegasHelperTrackUnselectedException)
+            {
+                MessageBox.Show("ビデオトラックが選択されていません。");
+            }
+            catch (VegasHelperNoneEventsException)
+            {
+                MessageBox.Show("選択したビデオトラック中にイベントが存在していません。");
+            }
+        }
+    }
+}
---
+++ VegasScriptRemoveJimakuPrefix/VegasHelperException.cs
@@ -0,0 +1,16 @@
+﻿using ScriptPortal.Vegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegasScriptRemoveJimakuPrefix
+{
+    internal class VegasHelperException : Exception { }
+    internal class VegasHelperNotFoundTrackException : VegasHelperException { }
+    internal class VegasHelperTrackUnselectedException : VegasHelperException { }
+    internal class VegasHelperNoneEventsException : VegasHelperException { }
+    internal class VegasHelperNotFoundOFXParameterException : VegasHelperException { }
+    internal class VegasHelperNotFoundJimakuPrefixException : VegasHelperException { }
+}
