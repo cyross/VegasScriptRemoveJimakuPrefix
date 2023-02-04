fdb07793422a4fed40909337d69b49a82f2990b2
Cyross Makoto <takuan45189@gmail.com>
2023-02-04 21:18:41 +0900

    フォームを追加

--- VegasScriptRemoveJimakuPrefix/EntryPoint.cs
+++ VegasScriptRemoveJimakuPrefix/EntryPoint.cs
@@ -1,5 +1,7 @@
 ﻿using ScriptPortal.Vegas;
 using VegasScriptHelper;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace VegasScriptRemoveJimakuPrefix
@@ -10,9 +12,28 @@ namespace VegasScriptRemoveJimakuPrefix
         {
             VegasHelper helper = VegasHelper.Instance(vegas);
 
+            Dictionary<string, VideoTrack> keyValuePairs = helper.GetVideoKeyValuePairs();
+
+            if(!keyValuePairs.Any())
+            {
+                MessageBox.Show("ビデオトラックがありません");
+            }
+
+            List<string> videoTrackKeys = keyValuePairs.Keys.ToList();
+
+            VideoTrack selected = helper.SelectedVideoTrack(false);
+
+            string initialKey = selected != null ? helper.GetTrackKey(selected) : videoTrackKeys[0];
+
+            SettingForm form = new SettingForm()
+            {
+                RemoveJimakuDataSource = videoTrackKeys,
+                RemoveJimakuTrackName = initialKey
+            };
+
             try
             {
-                helper.DeleteJimakuPrefix();
+                helper.DeleteJimakuPrefix(keyValuePairs[form.RemoveJimakuTrackName]);
             }
             catch (VegasHelperTrackUnselectedException)
             {
