2e2099e3536fe277c355e2cf7e06706324290d52
Cyross Makoto <takuan45189@gmail.com>
2023-02-06 21:46:20 +0900

    安定性の向上

--- VegasScriptRemoveJimakuPrefix/EntryPoint.cs
+++ VegasScriptRemoveJimakuPrefix/EntryPoint.cs
@@ -3,6 +3,8 @@ using VegasScriptHelper;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System;
 
 namespace VegasScriptRemoveJimakuPrefix
 {
@@ -12,31 +14,34 @@ namespace VegasScriptRemoveJimakuPrefix
 
         public void FromVegas(Vegas vegas)
         {
-            VegasHelper helper = VegasHelper.Instance(vegas);
-
-            Dictionary<string, VideoTrack> keyValuePairs = helper.GetVideoKeyValuePairs();
-
-            if(!keyValuePairs.Any())
-            {
-                MessageBox.Show("ビデオトラックがありません");
-            }
-
-            List<string> videoTrackKeys = keyValuePairs.Keys.ToList();
-
-            VideoTrack selected = helper.SelectedVideoTrack(false);
-
-            string initialKey = selected != null ? helper.GetTrackKey(selected) : videoTrackKeys[0];
-
-            if(settingForm == null) { settingForm = new SettingForm(); }
-
-            settingForm.RemoveJimakuDataSource = videoTrackKeys;
-            settingForm.RemoveJimakuTrackName = initialKey;
-
-            if(settingForm.ShowDialog() == DialogResult.Cancel) { return; }
-
             try
             {
-                helper.DeleteJimakuPrefix(keyValuePairs[settingForm.RemoveJimakuTrackName]);
+                VegasHelper helper = VegasHelper.Instance(vegas);
+
+                Dictionary<string, VideoTrack> keyValuePairs = helper.GetVideoKeyValuePairs();
+
+                if (!keyValuePairs.Any())
+                {
+                    MessageBox.Show("ビデオトラックがありません");
+                }
+
+                List<string> videoTrackKeys = keyValuePairs.Keys.ToList();
+
+                VideoTrack selected = helper.SelectedVideoTrack(false);
+
+                string initialKey = selected != null ? helper.GetTrackKey(selected) : videoTrackKeys[0];
+
+                if (settingForm == null) { settingForm = new SettingForm(); }
+
+                settingForm.RemoveJimakuDataSource = videoTrackKeys;
+                settingForm.RemoveJimakuTrackName = initialKey;
+
+                if (settingForm.ShowDialog() == DialogResult.Cancel) { return; }
+
+                using (new UndoBlock("字幕の接頭辞を削除"))
+                {
+                    helper.DeleteJimakuPrefix(keyValuePairs[settingForm.RemoveJimakuTrackName]);
+                }
             }
             catch (VegasHelperTrackUnselectedException)
             {
@@ -45,6 +50,19 @@ namespace VegasScriptRemoveJimakuPrefix
             catch (VegasHelperNoneEventsException)
             {
                 MessageBox.Show("選択したビデオトラック中にイベントが存在していません。");
+            }
+            catch (Exception ex)
+            {
+                string errMessage = "[MESSAGE]" + ex.Message + "\n[SOURCE]" + ex.Source + "\n[STACKTRACE]" + ex.StackTrace;
+                Debug.WriteLine("---[Exception In Helper]---");
+                Debug.WriteLine(errMessage);
+                Debug.WriteLine("---------------------------");
+                MessageBox.Show(
+                    errMessage,
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                throw ex;
             }
         }
     }
